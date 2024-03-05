a7cdbc7910578e542d878f9e96a75d08cd383d55
Andrew Arnott <andrewarnott@live.com>
2024-03-04 18:07:14 -0700

    Add project files.

---
+++ AvaloniaApplication1/ViewModels/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Nerdbank.Bitcoin;
+using Nerdbank.Zcash;
+
+namespace AvaloniaApplication1.ViewModels;
+
+public class MainViewModel : ViewModelBase
+{
+	public string Greeting { get; }
+
+	public MainViewModel()
+	{
+		ZcashAccount account = new(new Zip32HDWallet(Bip39Mnemonic.Create(256), ZcashNetwork.TestNet));
+		this.Greeting = $"Welcome to Avalonia: {account.DefaultAddress}";
+	}
+}
